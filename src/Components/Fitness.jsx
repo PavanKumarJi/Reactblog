d57e9beaf7620a5a4320fcf73c9e94bb7b9fb171
PavanKumarJi <100858300+PavanKumarJi@users.noreply.github.com>
2022-05-27 19:25:33 +0530

    Create Fitness.jsx

---
+++ src/Components/Fitness.jsx
@@ -0,0 +1,73 @@
+import React, { useContext } from "react";
+import { Data2 } from "./Fapi";
+import "./style.css";
+
+const Fitness = () => {
+  const [fitness, setFitness] = useContext(Data2);
+  return (
+    <>
+      <h2 className="fit">Fitness</h2>
+
+      <h2 className="right-title">Top Most</h2>
+      {fitness.map((data) => (
+        <div className="main-card" key={data.id}>
+          <div className="cards">
+            <div>
+              <img className="card-1" src={data.ImageAsset} alt="Img" />
+            </div>
+            <div className="card-2">
+              <div>
+                <h4 style={{ marginLeft: "1rem" }}>{data.Title}</h4>
+              </div>
+              <div>
+                <p style={{ marginLeft: "0.5rem" }}>{data.BlogContent}</p>
+              </div>
+              <div>
+                <p style={{ marginLeft: "0.5rem" }}>
+                  <span
+                    style={{ fontWeight: "bold", padding: "0 10px 0 10px" }}
+                  >
+                    {data.CategoryName} /
+                  </span>
+                  {data.PublishedDate}
+                </p>
+              </div>
+            </div>
+            <hr />
+          </div>
+
+          <div className="top-most">
+            <div>
+              <img
+                className="right-card"
+                src={data.ImageAsset}
+                alt="BollyImg"
+              />
+              <div>
+                <h3 className="title2">{data.Title}</h3>
+              </div>
+              <div className="title3">
+                <p>
+                  <span style={{ fontWeight: "bold" }}>
+                    {data.CategoryName} /
+                  </span>
+                  {data.PublishedDate}
+                </p>
+              </div>
+              <hr className="hr" />
+            </div>
+          </div>
+        </div>
+      ))}
+
+      <div className="add">
+        <img
+          src="https://res.cloudinary.com/dlawbbgbn/image/upload/v1653560887/advertisement-designing-services-500x500_h6igrm.jpg"
+          alt="Advartisment"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Fitness;
