6f11bde9eae7de100d9634d4081d58fbf79935cd
PavanKumarJi <100858300+PavanKumarJi@users.noreply.github.com>
2022-05-27 19:17:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Bollywood from "./Components/Bollywood";
+import Fitness from "./Components/Fitness";
+import Head from "./Components/Head";
+import Api from "./Components/Api";
+import Fapi from "./Components/Fapi";
+import Food from "./Components/Food";
+import Home from "./Components/Home";
+import Tourism from "./Components/Tourism";
+import Foodapi from "./Components/Foodapi";
+import Tourapi from "./Components/Tourapi";
+import Latestapi from "./Components/Latestapi";
+import Topapi from "./Components/Topapi";
+import Articalapi from "./Components/Articalapi";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Topapi>
+      <Articalapi>
+        <Latestapi>
+          <Tourapi>
+            <Foodapi>
+              <Fapi>
+                <Api>
+                  <>
+                    <BrowserRouter>
+                      <Head />
+                      <Routes>
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/torisum" element={<Tourism />} />
+                        <Route path="/bollywood" element={<Bollywood />} />
+                        <Route path="/fitness" element={<Fitness />} />
+                        <Route path="/food" element={<Food />} />
+                      </Routes>
+                    </BrowserRouter>
+                  </>
+                </Api>
+              </Fapi>
+            </Foodapi>
+          </Tourapi>
+        </Latestapi>
+      </Articalapi>
+    </Topapi>
   );
 }
 
